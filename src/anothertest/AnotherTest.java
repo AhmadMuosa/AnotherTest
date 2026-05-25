90cb5c35ae551e06a29663630bd0dfe454b7478e
AhmadMuosa <muosa@sheridancollege.ca>
2026-05-25 10:01:35 -0400

    Update AnotherTest.java
    new field var

--- src/anothertest/AnotherTest.java
+++ src/anothertest/AnotherTest.java
@@ -16,6 +16,7 @@ public class AnotherTest {
     public static void main(String[] args) {
         // TODO code application logic here
         //Modified for pull
+        private String name;
     }
     
 }
